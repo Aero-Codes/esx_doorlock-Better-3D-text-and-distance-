88aa8802807711cd5da038503b25a8fa81d4b4b8
thelindat <65407488+thelindat@users.noreply.github.com>
2021-01-20 21:44:45 +1100

    Update and rename __resource.lua to fxmanifest.lua

--- esx_doorlock/fxmanifest.lua
+++ esx_doorlock/fxmanifest.lua
@@ -1,6 +1,8 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+games { 'gta5' }
 
-description 'ESX Door Lock'
+description 'thelindat esx_doorlock'
+version '1.0.0'
 
 server_scripts {
 	'@es_extended/locale.lua',
